d52819050cca1532293c9fe6c636446fd1658c4d
Vihar Shah <svihar3108@gmail.com>
2024-03-20 19:00:43 +0530

    Kafka and Elasticsearch start-stop script

--- kafka&ES_Files/runKafkaES.sh
+++ kafka&ES_Files/runKafkaES.sh
@@ -15,11 +15,22 @@ else
     zkServer restart
 fi
 
-# Start Kafka server with KafkaServer.properties
-echo "Starting Kafka Server..."
-kafka-server-start KafkaServer.properties > kafka.log &
-echo "Kafka server running on pid: $!"
 
-echo "Starting elasticsearch..."
-elasticsearch --daemonize
-echo  "elasticsearch running on pid: $(pgrep -f elasticsearch)"
+# Start Kafka server with KafkaServer.properties
+# Will match kafka and zookeeper
+if [ "$(pgrep -f "kafka" | wc -l)" -eq 0 ]; then
+    echo "Starting Kafka Server..."
+    kafka-server-start KafkaServer.properties > kafka.log &
+    echo "Kafka server running on pid: $!"
+else
+  echo "kafka already running"
+  pgrep -f "kafka" | wc -l
+fi
+
+
+# Start elasticsearch in daemon process
+if [ "$(pgrep -f "elasticsearch" | wc -l)" -eq 0 ]; then
+    echo "Starting elasticsearch..."
+    elasticsearch --daemonize
+    echo  "elasticsearch running on pid: $(pgrep -f elasticsearch)"
+fi
---
+++ kafka&ES_Files/stopKafkaES.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+pkill_steps() {
+  echo "force killing elasticsearch..."
+  pkill -15 -f elasticsearch
+  echo "force killing kafka..."
+  pkill -15 -f kafka
+  echo "force killing zookeeper..."
+  pkill -15 -f zookeeper
+}
+
+# Stop elasticsearch server if running
+ES_PID=$(pgrep -f "elasticsearch")
+if [ -n "$ES_PID" ]; then
+  echo "stopping elasticsearch..."
+  for pid in $ES_PID; do
+    kill -15 "$pid"
+  done
+fi
+
+
+# Stop Kafka Server if running
+if [ "$(pgrep -f kafka | wc -l)" -ne 0 ]; then
+    echo "stopping kafka server..."
+    kafka-server-stop
+fi
+
+
+# Stop Zookeeper if running
+ZK_STATUS=$(pgrep -f "zookeeper" | wc -l)
+
+if [ "$ZK_STATUS" -ne 0 ]; then
+    echo "Stopping ZooKeeper Server..."
+    # Start ZooKeeper (replace with your actual ZooKeeper start command)
+    zkServer stop
+fi
