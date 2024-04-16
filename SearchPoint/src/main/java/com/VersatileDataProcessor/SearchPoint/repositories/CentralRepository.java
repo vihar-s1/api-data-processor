8952e8d020d41717077689e16b3a9687ae83742a
Vihar Shah <svihar3108@gmail.com>
2024-04-16 23:00:40 +0530

    random new endpoints

--- SearchPoint/src/main/java/com/VersatileDataProcessor/SearchPoint/repositories/CentralRepository.java
+++ SearchPoint/src/main/java/com/VersatileDataProcessor/SearchPoint/repositories/CentralRepository.java
@@ -3,12 +3,13 @@ package com.versatileDataProcessor.searchPoint.repositories;
 import com.versatileDataProcessor.searchPoint.models.MessageType;
 import com.versatileDataProcessor.searchPoint.models.StandardMessage;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 
 public interface CentralRepository extends ElasticsearchRepository<StandardMessage, String> {
-    public List<StandardMessage> findAllByMessageType(MessageType messageType, Pageable pageable);
+    List<StandardMessage> findAllByMessageType(MessageType messageType, Pageable pageable);
+    List<StandardMessage> findAllByTagsContainingIgnoreCase(String tag, Pageable pageable);
+    List<StandardMessage> findAllByNameContaining(String name, Pageable pageable);
 }
