d1b2bf21ca76c8ca5167d082e28a6824e7f1da5e
Vihar Shah <96971096+vihar-s1@users.noreply.github.com>
2024-06-12 14:25:56 +0530

    Security Config (#3)
    * elasticsearch security config
    
    * Regex Manager Security Config
    
    - added authentication to regex manager
    
    * fixed regex manager build.gradle

---
+++ RegexManager/src/main/java/com/VersatileDataProcessor/RegexManager/config/SecurityConfig.java
@@ -0,0 +1,72 @@
+package com.VersatileDataProcessor.RegexManager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    /*
+     The Web-Security configurations are evaluated first in the filter chain, so
+     they take precedence over the Http-Security configurations.
+    */
+
+    /* WEB-SECURITY BEANS HERE --> HIGHER PRECEDENCE THAN HTTP SECURITY */
+    @Bean
+    public HttpFirewall allowedHttpMethods() {
+        List<String> allowedMethods = new ArrayList<>();
+        allowedMethods.add("GET");
+        allowedMethods.add("POST");
+        allowedMethods.add("DELETE");
+
+        StrictHttpFirewall strictHttpFirewall = new StrictHttpFirewall();
+        strictHttpFirewall.setAllowedHttpMethods(allowedMethods);
+        return strictHttpFirewall;
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.withUsername("user")
+                .password(passwordEncoder.encode("password"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder.encode("password"))
+                .roles("USER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public SecurityFilterChain getSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(request ->
+                    request.requestMatchers("/restricted/**").hasRole("ADMIN")
+                            .requestMatchers("/api/**").permitAll()
+                            .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
